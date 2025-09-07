63c04ebb19bd059f74adcdb6c8aa855161223cc9
brojelly <lhm4802@nate.com>
2025-09-07 19:00:42 +0900

    TODO 상세 조회

--- todo/views.py
+++ todo/views.py
@@ -7,3 +7,6 @@ def todo_list(request):
     todos = Todo.objects.filter(complete=False)
     return render(request, 'todo/todo_list.html', {'todos': todos})
 
+def todo_detail(request, pk):
+    todo = Todo.objects.get(id=pk)
+    return render(request, 'todo/todo_detail.html', {'todo': todo})
